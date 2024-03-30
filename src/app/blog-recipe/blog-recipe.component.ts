1e39f5f996078484b025e9e4074b666962b1bd9d
Alex <areichel1986@gmail.com>
2024-03-30 15:30:33 +0100

    ingrediens adding possible

--- src/app/blog-recipe/blog-recipe.component.ts
+++ src/app/blog-recipe/blog-recipe.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators, FormArray, AbstractControl } from '@angular/forms';
 
-import { Recipe } from './recipe.model';
+import { Recipe, Ingredient } from './recipe.model';
 import { RecipeService } from './recipe.service';
 
 @Component({
@@ -9,9 +9,14 @@ import { RecipeService } from './recipe.service';
   templateUrl: './blog-recipe.component.html',
   styleUrls: ['./blog-recipe.component.css']
 })
+
+
+
+
 export class BlogRecipeComponent implements OnInit {
   recipes: Recipe[] = [];
   blogRecipeForm!: FormGroup;
+  
 
   constructor(private recipeService: RecipeService, private formBuilder: FormBuilder) { }
 
@@ -31,14 +36,38 @@ export class BlogRecipeComponent implements OnInit {
     this.blogRecipeForm = this.formBuilder.group({
       title: ['xcvxcv', Validators.required], // Define form controls and validators
       content: ['xcvxcv', Validators.required],
-      img_url: ['xcvxcv', Validators.required]
+      img_url: ['xcvxcv', Validators.required],
+      ingredients: this.formBuilder.array([]) // Initialize ingredients as a FormArray
+
     });
   }
 
-  async addNewBlogPost(title: string, content: string, img_url: string): Promise<void> {
-    const url = 'http://localhost:3000/recipes'; // Your server endpoint URL
-    const data = { title, content, img_url }; // Data of the new blog recipe
+  get ingredientsArray() {
+    return this.blogRecipeForm.get('ingredients') as FormArray;
+  }
 
+  addIngredient(): void {
+    // Add a new FormGroup for the ingredient with default values
+    this.ingredientsArray.push(
+      this.formBuilder.group({
+        name: ['', Validators.required],
+        amount: ['', Validators.required]
+      })
+    );
+  }
+
+  removeIngredient(index: number): void {
+    if (this.ingredientsArray && this.ingredientsArray.length > index) {
+      this.ingredientsArray.removeAt(index);
+    }
+  }
+
+ 
+
+  async addNewBlogPost(title: string, content: string, img_url: string, ingredients: Ingredient[]): Promise<void> {
+    const url = 'http://localhost:3000/recipes'; // Your server endpoint URL
+    const data = { title, content, img_url, ingredients }; // Data of the new blog recipe
+    console.log("async function",data)
     try {
       const response = await fetch(url, {
         method: 'POST',
@@ -62,14 +91,17 @@ export class BlogRecipeComponent implements OnInit {
   }
 
   onSubmit(): void {
-    const { title, content, img_url } = this.blogRecipeForm.value;
-    this.addNewBlogPost(title, content, img_url)
-      .then(() => {
-        // Additional actions after successful addition of the blog recipe
-      })
-      .catch(error => {
-        // Handle error
-      });
+    if (this.blogRecipeForm.valid) {  
+        const { title, content, img_url, ingredients } = this.blogRecipeForm.value;
+        console.log("this are the ingredients",this.blogRecipeForm.value)
+        this.addNewBlogPost(title, content, img_url, ingredients)
+            .then(() => {
+                // Erfolgreich hinzugefügt
+            })
+            .catch(error => {
+                // Fehlerbehandlung
+            });
+    }
   }
 }
 
---
+++ src/app/recipe-list/recipe-list.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../blog-recipe/recipe.model';
+import { RecipeService } from '../blog-recipe/recipe.service';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrl: './recipe-list.component.css'
+})
+export class RecipeListComponent {
+  recipes: Recipe[] = [];
+
+  constructor(private recipeService: RecipeService) { }
+
+  ngOnInit(): void {
+    this.getRecipes();
+
+  }
+
+  getRecipes(): void {
+    this.recipeService.getRecipes()
+      .subscribe(recipes => this.recipes = recipes);
+  }
+  // onDeleteRecipe(postId: string): void {
+  //   this.recipeService.deleteRecipe(postId).subscribe(() => {
+  //     // Update the list of recipes after deletion
+  //     this.getRecipes();
+  //   });
+  // }
+}
