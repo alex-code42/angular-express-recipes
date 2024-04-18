b4e7d275d4617c0e6394dae11f59354d2ce6bab5
Alex <areichel1986@gmail.com>
2024-04-18 14:43:00 +0200

    yhe

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { HomeComponent } from './pages/home/home.component';
 import { AboutComponent } from './pages/about/about.component';
 import { ShoppingListComponent } from './pages/shopping-list/shopping-list.component';
 
+
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
