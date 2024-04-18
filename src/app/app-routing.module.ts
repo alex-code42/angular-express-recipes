5a4c606060c5be837f5be5a06490e7a1622745ff
Alex <areichel1986@gmail.com>
2024-04-18 13:58:02 +0200

    nomore bugs

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+import { AboutComponent } from './about/about.component';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'about', component: AboutComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
