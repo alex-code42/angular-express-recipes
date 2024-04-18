00f022e3f659d4a71cd9059744aedc0cab5b33c8
Alex <areichel1986@gmail.com>
2024-04-18 13:33:42 +0200

    comments work again and look nice

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent {
+  showRecipeForm: boolean = false;
+  buttonText: string = 'Create Recipe'; // Initial button text
+  buttonColor: string = 'btn-success'; // Initial button color
+
+
+
+  showBlogRecipe: boolean = false;
+
+  toggleBlogRecipe(): void {
+    // Toggle the value of showBlogRecipe
+    this.showBlogRecipe = !this.showBlogRecipe;
+  }
+}
